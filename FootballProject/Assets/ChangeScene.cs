aeb0aef7b1c440fcc2036efd107e8fea81bc9c85
Levi Rosenberg <s179066@students.aie.edu.au>
2018-02-12 12:44:15 -0800

    Main Menu is Functioning

---
+++ FootballProject/Assets/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string wool;
+	// Use this for initialization
+
+	// Update is called once per frame
+	public void gotoScene()
+    {
+        SceneManager.LoadScene(wool);
+   
+	}
+    public void quitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
